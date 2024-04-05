366b45097764d7425a7052ab48fcc73fb5035723
Unnikrishnan Menon <menon.uk1998@gmail.com>
2024-04-05 15:38:53 +0530

    testing paths
    Co-authored-by: Anirudh <senseianirudh28@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-# sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../ravnest'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # Configuration file for the Sphinx documentation builder.
 #
