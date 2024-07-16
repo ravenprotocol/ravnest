5abee2a160999ca933917263891c48e95815b51d
Anirudh Menon <senseianirudh28@gmail.com>
2024-07-16 13:59:10 +0530

    Ping endpoint in grpc server
    Co-authored-by: Unnikrishnan <menon.uk1998@gmail.com>

--- ravnest/endpoints.py
+++ ravnest/endpoints.py
@@ -1,5 +1,9 @@
 from .protos.server_pb2_grpc import CommServer
-from .protos.server_pb2 import CheckBufferStatus, BufferStatusReply, ReceivedChunk, ReduceChunk, GatherChunk, CheckReduceIteration, CheckGatherIteration, ReduceIterationReply, GatherIterationReply, SendLatestWeights, WeightsChunk
+from .protos.server_pb2 import (CheckBufferStatus, BufferStatusReply, ReceivedChunk, 
+                                ReduceChunk, GatherChunk, CheckReduceIteration, 
+                                CheckGatherIteration, ReduceIterationReply, 
+                                GatherIterationReply, SendLatestWeights, 
+                                WeightsChunk, PingRequest, PingResponse)
 from .protos.tensor_pb2 import SendTensor, SendTensorReply
 from .utils import generate_weights_stream
 
@@ -148,3 +152,6 @@ class GrpcService(CommServer):
         send_dict = {k:latest_state_dict[k] for k in state_dict_keys[key_start_index:key_end_index+1]}
         self.latest_weights_lock.release()
         return generate_weights_stream(send_dict)
+    
+    def Ping(self, request:PingRequest, context) -> PingResponse:
+        return PingResponse(data="Pong")
