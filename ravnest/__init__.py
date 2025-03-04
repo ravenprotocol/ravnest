1face1fa01d77a8a80e63ade5e7275a5858950de
Anirudh Menon <senseianirudh28@gmail.com>
2025-03-04 16:47:17 +0530

    Inference engine import fixed

--- ravnest/__init__.py
+++ ravnest/__init__.py
@@ -3,6 +3,7 @@ from .communication import *
 from .compute import *
 from .endpoints import *
 from .node_tcp import *
+from .inference_engine import InferenceEngine
 from .strings import *
 from .trainer import *
 from .utils import *
